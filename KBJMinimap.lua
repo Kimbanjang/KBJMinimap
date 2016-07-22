bafdfe132bbeb465f506a964e13d1061e1b27fb2
Kimbanjang <dhejrgn@gmail.com>
2016-07-22 13:14:02 +0900

    Change TOC for 7.0.3

--- KBJMinimap.lua
+++ KBJMinimap.lua
@@ -151,26 +151,26 @@ local cfg = {}
 cfg.y = 150
 
 local function AdjustSetPoint(self,...)
-  local a1,af,a2,x,y = ...
-  if a1 and af == "MinimapCluster" and y ~= cfg.y then    
-    if not InCombatLockdown() then
-      self:SetPoint(a1,af,a2,x,cfg.y)
-    else
-      frame.point = {a1,af,a2,x,cfg.y}
-      frame:RegisterEvent("PLAYER_REGEN_ENABLED")
-    end      
-  end
+    local a1,af,a2,x,y = ...
+    if a1 and af == "MinimapCluster" and y ~= cfg.y then    
+        if not InCombatLockdown() then
+            self:SetPoint(a1,af,a2,x,cfg.y)
+        else
+            frame.point = {a1,af,a2,x,cfg.y}
+            frame:RegisterEvent("PLAYER_REGEN_ENABLED")
+        end      
+    end
 end 
 
 frame:SetScript("OnEvent", function(self,event)
-  self:UnregisterEvent(event)
-  if event == "PLAYER_LOGIN" then
-    self.point = {ObjectiveTrackerFrame:GetPoint()}      
-    hooksecurefunc(ObjectiveTrackerFrame, "SetPoint", AdjustSetPoint)    
-  end
-  if not InCombatLockdown() then
-    ObjectiveTrackerFrame:SetPoint(unpack(self.point))
-  end
+    self:UnregisterEvent(event)
+    if event == "PLAYER_LOGIN" then
+        self.point = {ObjectiveTrackerFrame:GetPoint()}      
+        hooksecurefunc(ObjectiveTrackerFrame, "SetPoint", AdjustSetPoint)    
+    end
+    if not InCombatLockdown() then
+        ObjectiveTrackerFrame:SetPoint(unpack(self.point))
+    end
 end)
 
 frame:RegisterEvent("PLAYER_LOGIN")
