04d7b400702ca0b421100bd381ccd0c53be1ebb7
Kimbanjang <dhejrgn@gmail.com>
2016-08-26 23:39:12 +0900

    Remove ObjectiveTracker i used rObjectiveTracker now

--- KBJMinimap.lua
+++ KBJMinimap.lua
@@ -141,38 +141,3 @@ Minimap:SetScript("OnMouseUp", function(self, btn)
 		Minimap_OnClick(self)
 	end
 end)
-
-
--- ObjectiveTracker Move
-local an, at = ...
-local unpack = unpack  
-local ObjectiveTrackerFrame = ObjectiveTrackerFrame
-
-local frame = CreateFrame("Frame")
-
-local cfg = {}
-cfg.y = 150
-
-local function AdjustSetPoint(self,...)
-    local a1,af,a2,x,y = ...
-    if a1 and af == "MinimapCluster" and y ~= cfg.y then    
-        if not InCombatLockdown() then
-            self:SetPoint(a1,af,a2,x,cfg.y)
-        else
-            frame.point = {a1,af,a2,x,cfg.y}
-            frame:RegisterEvent("PLAYER_REGEN_ENABLED")
-        end      
-    end
-end 
-
-frame:SetScript("OnEvent", function(self,event)
-    self:UnregisterEvent(event)
-    if event == "PLAYER_ENTERING_WORLD" then
-        self.point = {ObjectiveTrackerFrame:GetPoint()}      
-        hooksecurefunc(ObjectiveTrackerFrame, "SetPoint", AdjustSetPoint)    
-    end
-    if not InCombatLockdown() then
-        ObjectiveTrackerFrame:SetPoint(unpack(self.point))
-    end
-end)
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
